016a7bede89337ade80e72788d2679411fe8c223
fathimaekaksha-alt <fathima.ekaksha@gmail.com>
2025-10-16 17:54:36 +0530

    Fifth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:grocery_app/firebase_options.dart';
 import 'package:grocery_app/screens/splashscreen.dart';
 
 
 void main()async {
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MyApp());
 }
 
